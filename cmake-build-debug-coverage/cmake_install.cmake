48b526481a424bfabd5b52b0d29c5df076c3e34b
arscholtes <alex.scholtes@icloud.com>
2024-08-19 12:04:54 -0400

    check

--- cmake-build-debug-coverage/cmake_install.cmake
+++ cmake-build-debug-coverage/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/onyx/CLionProjects/untitled
+# Install script for directory: /Users/onyx/CLionProjects/JYM1-ClassRoster
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/onyx/CLionProjects/untitled/cmake-build-debug-coverage/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/onyx/CLionProjects/JYM1-ClassRoster/cmake-build-debug-coverage/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
